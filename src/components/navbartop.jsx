c99933488720ba841c7be3954c3271fe414fd41f
Natalia <natalia.avivnotario@gmail.com>
2020-11-23 21:16:53 -0300

    carousel

--- src/components/navbartop.jsx
+++ src/components/navbartop.jsx
@@ -30,10 +30,12 @@ function Navbar() {
 
         </ul>
         <form className="form-inline my-2 my-lg-0">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> </input>
             <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
 </nav>
+  )
+}
 
 export default Navbar;
