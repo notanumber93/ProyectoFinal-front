edc281077c01294cf654f691749e77832989bde8
jvera <vera.j.veas@gmail.com>
2020-12-08 10:39:16 -0300

    update perfil y login/signup

---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,86 @@
+import { useContext, useState } from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { Context } from "../store/appContext";
+
+const UserProfile = (props) => {
+    const { store, actions } = useContext(Context);
+    const [bio, setBio] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(bio);
+        actions.updateBio(bio);
+    };
+
+    return (
+        <Container>
+            <h1>Perfil</h1>
+            <Form onSubmit={(e) => handleSubmit(e)}>
+                <Form.Group as={Row}>
+                    <Form.Label column sm="5" style={{ textAlign: "center" }}>
+                        Nombre
+                    </Form.Label>
+                    <Col>
+                        <Form.Control
+                            plaintext
+                            readOnly
+                            defaultValue={props.firstName}
+                        />
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row}>
+                    <Form.Label column sm="5" style={{ textAlign: "center" }}>
+                        Apellido
+                    </Form.Label>
+                    <Col>
+                        <Form.Control
+                            plaintext
+                            readOnly
+                            defaultValue={props.lastName}
+                        />
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row}>
+                    <Form.Label column sm="5" style={{ textAlign: "center" }}>
+                        Email
+                    </Form.Label>
+                    <Col>
+                        <Form.Control
+                            plaintext
+                            readOnly
+                            defaultValue={props.email}
+                        />
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row}>
+                    <Form.Label column sm="5" style={{ textAlign: "center" }}>
+                        User
+                    </Form.Label>
+                    <Col>
+                        <Form.Control
+                            plaintext
+                            readOnly
+                            defaultValue={props.userName}
+                        />
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row}>
+                    <Form.Label column sm="5" style={{ textAlign: "center" }}>
+                        Bio
+                    </Form.Label>
+                    <Col>
+                        <Form.Control
+                            as="textarea"
+                            defaultValue={props.bio}
+                            onChange={(e) => setBio(e.target.value)}
+                        />
+                    </Col>
+                </Form.Group>
+                <Button type="submit" style={{ float: "right" }}>
+                    Actualizar bio
+                </Button>
+            </Form>
+        </Container>
+    );
+};
+export default UserProfile;
--- src/store/flux.js
+++ src/store/flux.js
@@ -1,54 +1,128 @@
-import React, {useContext, useState, useEffect,} from "react";
-
-
+import React, { useContext, useState, useEffect } from "react";
 
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			user_data: { },
-			movieList: [],
-			movieDetails: { }
+    return {
+        store: {
+            user_data: {
+                userName: "",
+                email: "",
+                userPass: "",
+                firstName: "",
+                lastName: "",
+                isAdmin: false,
+                bio: "",
+            },
+            login_data: {
+                userLogin: "",
+                userPass: "",
+            },
+            auth_token: "",
+            movieList: [],
+            movieDetails: {},
+        },
+
+        actions: {
+            updateBio: (bio) => {
+                const store = getStore();
+                let { user_data } = store;
+                user_data.bio = bio;
+                setStore({ user_data });
+                console.log(store.user_data);
+                console.log(store.user_data.id);
+                getActions().updateUser();
 			},
-
-			actions: {
-				onChangeUser: (evento) => {
-					console.log(evento)
-
-				},
-				getMovieList: async (searchValue) => { console.log(searchValue)
-					const response = await fetch(
-					`http://www.omdbapi.com?s=${searchValue}&type=movie&apikey=70240a7d`
-					);
-					const json = await response.json();
-					console.log("--json--", json);
-					setStore({movieList: json.Search});
-
-			
-				},
-				getMovieDetails: async (title) => {
-					getActions().cleanMovieDetails();
-					const response = await fetch(
-						`https://cors-anywhere.herokuapp.com/http://www.omdbapi.com?t=${title}&apikey=70240a7d`
-					);
-					const json = await response.json();
-					console.log("--json--", json);
-					setStore({movieDetails: json});
-				},
-				cleanMovieDetails: () => {
-					setStore({movieDetails: { }});
-				}
-			}
-		
-		}
-	};
-	
+			updateUser: async () => {
+				const store = getStore();
+				let options = {
+					method: "PUT",
+					headers: {
+						"Content-Type": "application/json",
+						"Authorization": `Bearer ${store.auth_token}`,
+					},
+					body: JSON.stringify(store.user_data),
+				};
+				const response = await fetch(
+                    `http://127.0.0.1:5000/users/${store.user_data.id}`,
+                    options
+                );
+                const json = await response.json();
+                console.log(json);
+			},
+            onChangeUser: (evento) => {
+                const store = getStore();
+                const { user_data } = store;
+                user_data[evento.target.name] = evento.target.value;
+                setStore({ user_data });
+                console.log(store.user_data);
+            },
+            onSignup: async () => {
+                const store = getStore();
+                let options = {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(store.user_data),
+                };
+                const response = await fetch(
+                    "http://127.0.0.1:5000/signup",
+                    options
+                );
+                const json = await response.json();
+                console.log("--data--", json);
+            },
+            onChangeLogin: (evento) => {
+                const store = getStore();
+                const { login_data } = store;
+                login_data[evento.target.name] = evento.target.value;
+                setStore({ login_data });
+                console.log(store.login_data);
+            },
+            onLogin: async () => {
+                const store = getStore();
+                let options = {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(store.login_data),
+                };
+                const response = await fetch(
+                    "http://127.0.0.1:5000/login",
+                    options
+                );
+                const json = await response.json();
+                console.log("--data--", json);
+                let { user_data } = store;
+                user_data = { ...json.user };
+				setStore({ user_data });
+				setStore({ auth_token: json.access_token });
+				console.log(user_data);
+				console.log(store.auth_token);
+            },
+            getMovieList: async (searchValue) => {
+                console.log(searchValue);
+                const response = await fetch(
+                    `http://www.omdbapi.com?s=${searchValue}&type=movie&apikey=70240a7d`
+                );
+                const json = await response.json();
+                console.log("--json--", json);
+                setStore({ movieList: json.Search });
+            },
+            getMovieDetails: async (title) => {
+                getActions().cleanMovieDetails();
+                const response = await fetch(
+                    `http://www.omdbapi.com?t=${title}&apikey=70240a7d`
+                );
+                const json = await response.json();
+                console.log("--json--", json);
+                setStore({ movieDetails: json });
+            },
+            cleanMovieDetails: () => {
+                setStore({ movieDetails: {} });
+            },
+        },
+    };
+};
 
 export default getState;
-
-
-
-
-
-
-
-
