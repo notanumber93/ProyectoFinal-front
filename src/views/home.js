bba34adf860e50a1755d8092354e3ccb072eeaad
Natalia Aviv <52386108+notanumber93@users.noreply.github.com>
2020-12-19 13:02:42 -0300

    Update home.js

--- src/views/home.js
+++ src/views/home.js
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from "react";
 import Navbarhome from "../components/Navbarhome";
 import MyCarousel from "../components/MyCarousel";
+import MyCardDeck from "../components/MyCardDeck";
+import MyCard from "../components/MyCard";
 
 
-export const Home = () => {
+export const Home = (props) => {
    
     return (
         <>
             <Navbarhome />
 
             <MyCarousel />
+
+            <MyCardDeck title="Romance" searchValue="love"/>
+        
+
+
         </>
     );
 }
